141a3ffdac6a4e14bcc17083a2b3d353bf9ad82a
ivanclourenco <ivancampos039@gmail.com>
2022-06-17 09:59:05 -0300

    Desafio basico de js

---
+++ index.js
@@ -0,0 +1,27 @@
+function somar(numero1, numero2){
+    return  numero1 + numero2
+}
+resultadoDaSoma = somar( 5, 2)
+
+console.log("A soma de dois números é "+ resultadoDaSoma)
+
+function divisao(numero1, numero2){
+    return  numero1 / numero2
+}
+resultadoDaDivisao = divisao( 5, 2)
+
+console.log("A divisão de dois números é "+ resultadoDaDivisao)
+
+function multiplicacao(numero1, numero2){
+    return  numero1 * numero2
+}
+resultadoDaMultiplicacao = multiplicacao( 5, 2)
+
+console.log("A multiplicação de dois números é "+ resultadoDaMultiplicacao)
+
+function subtracao(numero1, numero2){
+    return  numero1 - numero2
+}
+resultadoDaSubtracao = subtracao( 5, 2)
+
+console.log("A subtração de dois números é "+ resultadoDaSubtracao)
